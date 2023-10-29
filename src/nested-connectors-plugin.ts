fdcd35354609a0cbd223d94a68721613a0d8c2e5
Nick Sethi <nicholas.sethi@gmail.com>
2023-10-28 23:51:16 -0700

    config prettier and eslint

---
+++ src/nested-connectors-plugin.ts
@@ -0,0 +1,34 @@
+import { GraphileConfig } from 'graphile-build';
+import { PgResource } from '@dataplan/pg';
+
+declare global {
+  namespace GraphileBuild {
+    interface Inflection {
+      nestedConnectByNodeIdField(
+        this: Inflection,
+        resource: PgResource
+      ): string;
+    }
+  }
+}
+
+const config: GraphileConfig.Plugin = {
+  name: '',
+  version: '',
+  description: '',
+  experimental: true,
+  provides: ['Nested Mutations'],
+  after: [''],
+  before: [''],
+  inflection: {
+    add: {},
+  },
+  gather: {},
+  schema: {},
+};
+// nestedConnectByNodeIdField(options, resource) {
+//         const { constraint } = options;
+//         if (!this) throw new Error("No inflector available")
+//         const fieldName = `connect_by_${constraint.keyAttributes.map((k) => this.column(k)).join("_and_")}`
+//         return this.camelCase(fieldName)
+//
